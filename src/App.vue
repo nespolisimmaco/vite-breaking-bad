4d3d6a8f44920cc6c3a5b7d8c3d028b79d6639e7
nespolisimmaco <snespoli@yahoo.com>
2023-04-17 18:00:11 +0200

    Add: get card objects from API with axios, and put them in cards array inside store

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,24 @@
 <script>
+import { store } from './store';
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  mounted() {
+    axios
+      .get(store.apiURL)
+      .then((resp) => {
+        console.log(resp);
+        const myData = resp.data.data;
+        console.log(myData);
+        this.store.cards = myData;
+      })
+  }
+}
 </script>
 
 <template></template>
